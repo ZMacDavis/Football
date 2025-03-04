1efff06086a0228392be2a558112966075523897
Zane Davis <zbaner@yahoo.com>
2025-03-04 14:33:24 -0700

    What is Football to you UI. Prof. Gallaugher CH 1.

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,10 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(Color.green)
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(Color.blue)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(Color.indigo)
+                Image(systemName: "figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(Color.purple)
+            }
+//            .padding(.leading)
+//            .padding(.trailing)
         }
         .padding()
     }
