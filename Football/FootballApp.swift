708c8da42a998c0d66243d3af56a215d2e8c66d8
Zane Davis <zbaner@yahoo.com>
2025-03-04 09:35:39 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Zane Davis on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
