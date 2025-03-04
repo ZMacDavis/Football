806916e49bbf2b3a1717faf1d1ddb88af0396270
Zane Davis <zbaner@yahoo.com>
2025-03-04 14:37:22 -0700

    Altering Main text field.

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("What is Football to You?")
+            Text("What is Football to You, hmm?")
                 .font(.largeTitle)
                 .fontWeight(.thin)
                 .foregroundStyle(Color.green)
